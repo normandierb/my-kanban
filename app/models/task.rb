6b1578f85d2cba51e0e1332dcdca2a2005e7dacc
Unicaen Dojo Team <dojo@normandierb.fr>
2013-11-25 23:19:02 +0100

    Déduplique le code contenant les états

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,7 @@
 class Task < ActiveRecord::Base
-	validates :state, inclusion: ["fait", "pas fait"]
+	OPTION_STATES=[["fait","fait"], ["pas fait","pas-fait"]]
+	VALID_STATES = OPTION_STATES.map{ |tab| tab[1] }.flatten
+	validates :state, inclusion: VALID_STATES
 	validates :title, presence: true, uniqueness: true
 	validates :description, presence: true
 end
